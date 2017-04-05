3113037477769f5529775e2495791a31d781d320
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-05 12:57:11 +0200

    Some useful conditions

---
+++ src/com/company/statementIf.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by Piotr on 2017-04-05.
+ */
+public class statementIf {
+    public static void main(String[] args) {
+
+        System.out.println(6 == 6);
+        System.out.println(1 != 11);
+        System.out.println(2 < 7);
+        System.out.println(15 > 100);
+
+    }
+}
