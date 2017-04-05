ce3aa579d2b70879bc1a0851d03fdea26a458a75
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-05 12:09:36 +0200

    test

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myNumer = 123;
+        short myShort = 999;
+        long myLong = 9897;
+
+        double myDouble = 8.3254663;
+        float myFloat = 254.5f;
+
+        char myChar = 'a';
+        boolean myBoolean = true;
+
+        byte myByte = 100;
+
+        System.out.println(myNumer);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+
+    }
+}
