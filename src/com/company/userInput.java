d510c838be9096934a2a859fa59c0d1711a5aca2
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-05 13:08:11 +0200

    Create scanner object

---
+++ src/com/company/userInput.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+/**
+ * Created by Piotr on 2017-04-05.
+ */
+public class userInput {
+    public static void main(String[] args) {
+        Scanner scInput = new Scanner(System.in);
+        
+    }
+}
