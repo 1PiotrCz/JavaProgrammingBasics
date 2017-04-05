87ff0339ec7faf162b8c0f05570387243d533df1
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-05 16:37:59 +0200

    difference between values and references

--- src/com/company/Arrays.java
+++ src/com/company/Arrays.java
@@ -5,9 +5,28 @@ package com.company;
  */
 public class Arrays {
     public static void main(String[] args) {
-        int value = 5;
+
         int[] values;
         values = new int[3];
-        
+
+        System.out.println(values[0]);
+
+        values[0] = 7;
+        values[1] = 20;
+        values[2] = 18;
+
+        System.out.println(values[0]);
+        System.out.println(values[1]);
+        System.out.println(values[2]);
+
+        for(int i=0; i < values.length; i++) {
+            System.out.println(values[i]);
+        }
+
+        int[] numbers = {7,6,5};
+
+        for(int i=0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
     }
 }
