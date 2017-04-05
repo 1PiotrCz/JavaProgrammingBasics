b061cd15897ae635b65b16461032275ec7f1d7fb
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-05 16:30:25 +0200

    Array definition.

---
+++ src/com/company/Arrays.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by Piotr on 2017-04-05.
+ */
+public class Arrays {
+    public static void main(String[] args) {
+        int value = 5;
+        int[] values;
+        values = new int[3];
+        
+    }
+}
