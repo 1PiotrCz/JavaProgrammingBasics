3dc9f5a10312bfb0a6e4b4f8512a3d89c0855f2e
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-05 12:27:11 +0200

    hello

---
+++ src/com/company/hello.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by Piotr on 2017-04-05.
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
