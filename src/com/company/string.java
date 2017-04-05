4354c2dfb612d614384944648df972954886cde6
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-05 12:36:09 +0200

    add some string.

---
+++ src/com/company/string.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by Piotr on 2017-04-05.
+ */
+public class string {
+    public static void main(String[] args) {
+
+        int myInt = 10;
+        String text = "Hello";
+        String space = " ";
+        String name = "Piotr";
+
+    }
+}
