5990be4feaae767e8c0f3de974589b20bfd405fc
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-05 12:47:04 +0200

    simple While loops.

---
+++ src/com/company/While.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by Piotr on 2017-04-05.
+ */
+public class While {
+    public static void main(String[] args) {
+
+        int value  = 0;
+
+        while(value < 10){
+            System.out.println(value + " " + "word");
+            value = value +1;
+        }
+    }
+}
