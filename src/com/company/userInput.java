3c40ff4425fbe8dd90b4106fdf79fb2361c092bb
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-05 13:12:43 +0200

    User enter value and display on screen this value.

--- src/com/company/userInput.java
+++ src/com/company/userInput.java
@@ -9,8 +9,10 @@ public class userInput {
     public static void main(String[] args) {
         Scanner scInput = new Scanner(System.in);
 
-        System.out.println("Enter a floating point value: ( example 2.3) ");
+        System.out.println("Enter a floating point value ( example 2.3 ) : ");
 
-        
+        double value = scInput.nextDouble();
+
+        System.out.println("Your value is : " + value);
     }
 }
