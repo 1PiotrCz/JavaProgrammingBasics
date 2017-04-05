999981ff9d310523fcc811c68e1a1b107fa9d07e
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-05 12:53:24 +0200

    simple for loops.

---
+++ src/com/company/ForLoops.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by Piotr on 2017-04-05.
+ */
+public class ForLoops {
+    public static void main(String[] args) {
+
+        for(int i=0; i < 5; i++){
+            System.out.println("Value of i is: " + i);
+        }
+    }
+}
