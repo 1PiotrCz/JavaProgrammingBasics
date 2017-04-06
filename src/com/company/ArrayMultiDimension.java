7e28caf098fc5a28f40b40db0c128e9209f2dfcc
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-06 12:44:21 +0200

    need 2 index to access values i 2d Array

--- src/com/company/ArrayMultiDimension.java
+++ src/com/company/ArrayMultiDimension.java
@@ -5,11 +5,18 @@ package com.company;
  */
 public class ArrayMultiDimension {
     public static void main(String[] args) {
-         int [] vaules = {2,7,5,24,55};
+
+        int [] vaules = {2,7,5,24,55};
         System.out.println(vaules[2]);
-        
 
+        int[][] grid = {
+                {3, 5, 43},
+                {2, 9},
+                {5, 4, 3, 2}
+        };
 
+        System.out.println(grid[1][1]);
+        System.out.println(grid[0][2]);
 
     }
 }
