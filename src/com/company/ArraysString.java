d61e3f59b941f1f57a9a760439d4e4b8e60bb6bb
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-06 12:07:48 +0200

    Declare array of string

---
+++ src/com/company/ArraysString.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by Piotr on 2017-04-06.
+ */
+public class ArraysString {
+    public static void main(String[] args) {
+
+        String[] words = new String[5];
+    }
+}
