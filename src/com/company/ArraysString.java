e8463e161ead4a8b692d495e793cdeaa5a51920e
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-06 12:09:32 +0200

    set array elements at String

--- src/com/company/ArraysString.java
+++ src/com/company/ArraysString.java
@@ -7,5 +7,12 @@ public class ArraysString {
     public static void main(String[] args) {
 
         String[] words = new String[5];
+
+        words[0] = "Hello";
+        words[1] = "I";
+        words[2] = "am";
+        words[3] = "Piotr";
+
+        
     }
 }
