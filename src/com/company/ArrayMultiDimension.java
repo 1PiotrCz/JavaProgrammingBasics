6ecfb97e0556535dcf9b6e918dbae02b8ff6a404
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-06 13:00:59 +0200

    Show Array 2d use for each (row)

--- src/com/company/ArrayMultiDimension.java
+++ src/com/company/ArrayMultiDimension.java
@@ -18,5 +18,18 @@ public class ArrayMultiDimension {
         System.out.println(grid[1][1]);
         System.out.println(grid[0][2]);
 
+        String[][] texts = new String[2][3];
+        texts[0][1] = "Hello there";
+        System.out.println(texts[0][1]);
+
+        for(int row=0; row < grid.length; row++) {
+            for(int col=0; col < grid[row].length; col++) {
+                System.out.print(grid[row][col] + "\t");
+            }
+
+            System.out.println();
+        }
+
+
     }
 }
