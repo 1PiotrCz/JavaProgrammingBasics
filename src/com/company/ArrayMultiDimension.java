bddb4fe436471aac18a5ec8e3bf821bf77d7fecc
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-06 12:39:23 +0200

    1d array

---
+++ src/com/company/ArrayMultiDimension.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by Piotr on 2017-04-06.
+ */
+public class ArrayMultiDimension {
+    public static void main(String[] args) {
+         int [] vaules = {2,7,5,24,55};
+
+         
+
+    }
+}
