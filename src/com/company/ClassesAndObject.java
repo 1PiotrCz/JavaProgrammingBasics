773e285f95ce59ea4cd2d2efb8a66f619b7d754e
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-17 23:42:38 +0200

    Create a Person object using the Person class. Create a obcject after use "new"

--- src/com/company/ClassesAndObject.java
+++ src/com/company/ClassesAndObject.java
@@ -7,4 +7,12 @@ class Person {
 }
 
 public class ClassesAndObject {
+    public static void main(String[] args) {
+
+        Person person1 = new Person();
+        person1.name = "Peter X";
+        person1.age = 15;
+
+        
+    }
 }
